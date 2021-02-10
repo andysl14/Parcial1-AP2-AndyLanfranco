9c1e1c93967ffdf38d126ebcdc8054373cdf852c
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2021-02-10 18:56:58 -0400

    Creando la Entidad Articulo con sus atributos

--- Models/Articulos.cs
+++ Models/Articulos.cs
@@ -11,5 +11,16 @@ namespace Parcial1_AP2_AndyLanfranco.Models
         [Key]
         public int ProductoId { get; set; }
 
+        [Required(ErrorMessage ="Este Campo esta vacio")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El campo Existencia esta vacio")]
+        public int Existencia { get; set; }
+
+        [Required(ErrorMessage = "El campo Costo esta vacio")]
+        public decimal Costo { get; set; }
+
+        [Required(ErrorMessage = "El Campo Valor Inventario esta vacio")]
+        public decimal Valor_Inventario { get; set; }
     }
 }
