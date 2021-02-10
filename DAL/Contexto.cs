45d56a73347f89024e479c68f29c7fbaf502311a
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2021-02-10 19:04:38 -0400

    Creando la clase contexto con su constructor para el ConnectionString

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_AP2_AndyLanfranco.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_AP2_AndyLanfranco.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+    }
+}
